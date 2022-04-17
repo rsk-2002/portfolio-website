3dc8a4309295c700725809cc48acf946d13e76dd
Ravi Shanker Kushwaha <ravishankerk48@gmail.com>
2022-04-17 06:43:58 +0530

    home.html modified and add logo to skills

--- base/models.py
+++ base/models.py
@@ -28,6 +28,7 @@ class Comment(models.Model):
 class Skill(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField(null=True, blank=True)
+    logo = models.ImageField(null=True, default='R.png')
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
 
